02c523a07a60179b1e147d43a3be3fd56e93ff60
alex6061 <39526012+alex6061@users.noreply.github.com>
2021-12-24 18:45:43 +0300

    2

--- js/menu.js
+++ js/menu.js
@@ -1,9 +1,11 @@
 ( () => {
-    const menuBbtnRef = document.querySelector ("[data-menu-button]");
-    
-    menuBbtnRef.addEventlistener ("click", () => {
+        const menuBbtnRef = document.querySelector ("[data-menu-button]");
+            // const mobileMenuRef = document.querySelector ("data-menu")
+        menuBbtnRef.addEventlistener ("click", () => {
+            // const expanded = menuBtnRef.getAttribute ("aria-expanded") === "true" || false;
         
         menuBbtnRef.classList.toggle ("is-open");
-    
+            // menuBbtnRef.setAttribute("aria-expanded", !expanded);
+            // // mobileMenuRef.classList.toggle("is-open");
     });
 })();
