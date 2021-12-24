438a5838ae81f581678760423db066749c43ebcf
alex6061 <39526012+alex6061@users.noreply.github.com>
2021-12-24 23:46:51 +0300

    2

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,16 +1,3 @@
-// ( () => {
-//         const menuBbtnRef = document.querySelector ("[data-menu-button]");
-//             const mobileMenuRef = document.querySelector ("data-menu")
-//         menuBbtnRef.addEventlistener ("click", () => {
-//             const expanded = menuBtnRef.getAttribute ("aria-expanded") === "true" || false;
-        
-//         menuBbtnRef.classList.toggle ("is-open");
-//             menuBbtnRef.setAttribute("aria-expanded", !expanded);
-//              mobileMenuRef.classList.toggle("is-open");
-//     });
-// })();
-
-
 (() => {
     const mobileMenu = document.querySelector('.js-menu-container');
     const openMenuBtn = document.querySelector('.js-open-menu');
@@ -33,7 +20,8 @@
   
     // Закрываем мобильное меню на более широких экранах
     // в случае изменения ориентации устройства.
-    window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+
+      window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
       if (!e.matches) return;
       mobileMenu.classList.remove('is-open');
       openMenuBtn.setAttribute('aria-expanded', false);
